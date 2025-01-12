6be665c03fe155f72e147a2da22278af015906c9
Amer <a.mulabeg@proton.me>
2025-01-12 19:45:01 +0100

    i nuked my swayconfig

---
+++ .config/sway/toggle_display.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Check if DP-3 is connected
+if swaymsg -t get_outputs | jq -e '.[] | select(.name == "DP-3" and .active)' >/dev/null; then
+    # If DP-3 is connected, disable eDP-1 and enable DP-3
+    swaymsg output eDP-1 disable
+    swaymsg output DP-3 mode 1920x1080@119.993Hz enable
+else
+    # If DP-3 is not connected, enable eDP-1 and disable DP-3
+    swaymsg output eDP-1 enable
+fi
+
